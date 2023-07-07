dda4f1bdccc71eeeb23f651647099760b24ccea7
favour williams <hello.williamsfav@gmail.com>
2023-07-07 16:31:17 +0100

    added todo textfield

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-class Colors {
-  Color tdRed = Color(0xFFDA4040);
-  Color tdBlue = Color(0xFF5F52EE);
-  Color tdBlack = Color(0xFF3A3A3A);
-  Color tdGrey = Color(0xFF717171);
-  Color tdBGColor = Color(0xFFEEEFF5);
+class AppColors {
+  static Color tdRed = Color(0xFFDA4040);
+  static Color tdBlue = Color(0xFF5F52EE);
+  static Color tdBlack = Color(0xFF3A3A3A);
+  static Color tdGrey = Color(0xFF717171);
+  static Color tdBGColor = Color(0xFFEEEFF5);
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+import '../widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,11 +8,82 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Todo App"),
-      ),
+      backgroundColor: AppColors.tdBGColor,
+      appBar: buildAppBar(),
       body: Container(
-        child: Text("home screen"),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      top: 50,
+                      bottom: 20,
+                    ),
+                    child: const Text(
+                      "All ToDos",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  TodoItem(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: AppColors.tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: const BoxConstraints(
+            maxHeight: 20,
+            maxWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: "Search...",
+          hintStyle: TextStyle(color: AppColors.tdBlack),
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: AppColors.tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            size: 30,
+            color: AppColors.tdBlack,
+          ),
+          CircleAvatar(
+            backgroundColor: AppColors.tdBlack,
+          )
+        ],
       ),
     );
   }
