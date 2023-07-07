1dab5dd6e0aae419421bbaf1e7b448113a1546bd
favour williams <hello.williamsfav@gmail.com>
2023-07-07 16:32:09 +0100

    build todo item card

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/constants/colors.dart';
+
+class TodoItem extends StatelessWidget {
+  const TodoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListTile(
+        onTap: () {
+          print("clicked on todo item");
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box,
+          color: AppColors.tdBlue,
+        ),
+        title: Text(
+          "Check Mail",
+          style: TextStyle(
+            fontSize: 16,
+            color: AppColors.tdBlack,
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+        trailing: Container(
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: AppColors.tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.delete,
+              size: 18,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
