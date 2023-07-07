2ab41070f9de3b93df6d497030463ea2ffcbd3dc
favour williams <hello.williamsfav@gmail.com>
2023-07-07 15:10:10 +0100

    set up project

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  Color tdRed = Color(0xFFDA4040);
+  Color tdBlue = Color(0xFF5F52EE);
+  Color tdBlack = Color(0xFF3A3A3A);
+  Color tdGrey = Color(0xFF717171);
+  Color tdBGColor = Color(0xFFEEEFF5);
+}
