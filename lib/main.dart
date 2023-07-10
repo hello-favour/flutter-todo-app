3a844629eb740685a4429187251011cd339d6c4c
favour williams <hello.williamsfav@gmail.com>
2023-07-10 12:51:12 +0100

    build calculator in same file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_todo_app/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +10,120 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    // SystemChrome.setSystemUIOverlayStyle(
+    //     SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Todo App',
-      home: Home(),
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const MyHomePage(title: "Calculator"),
+    );
+  }
+}
+
+buttonPressed() {}
+
+Widget buildButton(String buttonText) {
+  return Expanded(
+    child: SizedBox(
+      height: 70.0,
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: OutlinedButton(
+          onPressed: () {
+            buttonPressed();
+          },
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerRight,
+              padding:
+                  const EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+              child: const Text(
+                "0",
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Expanded(
+              child: Divider(),
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    buildButton("7"),
+                    buildButton("8"),
+                    buildButton("9"),
+                    buildButton("/"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton("4"),
+                    buildButton("5"),
+                    buildButton("6"),
+                    buildButton("x"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton("1"),
+                    buildButton("2"),
+                    buildButton("3"),
+                    buildButton("-"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton("."),
+                    buildButton("0"),
+                    buildButton("00"),
+                    buildButton("+"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton("clear"),
+                    buildButton("="),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
